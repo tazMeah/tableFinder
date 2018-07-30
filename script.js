e0edad7bb0d62300a9fe5ed9e24fa8c2be340bbc
tazMeah <montazmeahii@yahoo.com>
2018-07-30 19:10:59 -0400

    changed to document.on and now everything is working as it should. Reserved tables cannot be clicked to display the form.

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
 "use strict";
-
+let change;
 $(document).ready(function(){
      
-    // add the class available to each table
-    $("#table div").each(function(){
-        $(this).addClass("available")
+   
+
+    // if user clicks an available table or the cancel button, the form toggles
+
+    // $("div.available, #cancel").click(function(e){     
+    //     $("#bottom").toggle()
+    //     change = e.target;
+    // })
+
+    $(document).on("click", "div.available", function(e){
+        $("#bottom").toggle();
+        // saves the button that opened the form
+         change = e.target; 
+    
     })
 
-    let change;
-    // if user clicks an available table or the cancel button, the form toggles
-    $("div.available, #cancel").click(function(e){     
-        $("#bottom").toggle()
-        change = e.target;
+    $("form").on("click", "#save", function(){
+        $("#bottom").toggle();
+        $(change).toggleClass("available reserved")
     })
+
+    $("form").on("click", "#cancel", function(){
+        $("#bottom").toggle();
+    })
+   
 
    // if the user clicks the save button, the form toggles, and the table changes color
 
-   $("#save").click(function(){
-        $(change).toggleClass("available reserved");
-        $("#bottom").toggle();
-   })
+//    $("#save").click(function(){
+//         $(change).toggleClass("available reserved");
+//         $("#bottom").toggle();
+//    })
 
 
 
